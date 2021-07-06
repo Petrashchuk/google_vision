24383a7a039aed1282764f6f63d8526be5fc10fe
Andrii Petrashchuk <triumph.town99@gmail.com>
2021-07-06 18:33:45 +0300

    fetch amazon api

--- src/routers/pants/index.js
+++ src/routers/pants/index.js
@@ -4,16 +4,20 @@ import fetch from "node-fetch";
 const router = new KoaRouter();
 
 router.get('/',async (ctx)=>{
-   const response = await fetch("https://apidojo-hm-hennes-mauritz-v1.p.rapidapi.com/products/detail?lang=en&productcode=0839915011&country=asia2", {
-       "method": "GET",
-       "headers": {
-           "x-rapidapi-key": "cc74283b3cmsha2d48cf300f3721p1c76adjsn38941aea8e35",
-           "x-rapidapi-host": "apidojo-hm-hennes-mauritz-v1.p.rapidapi.com"
-       }
-   })
-    const data = await response.json();
-    console.log(data);
-   ctx.body = JSON.stringify(data);
+    try {
+        const response = await fetch('https://amazon-price1.p.rapidapi.com/search?marketplace=ES&keywords=dress', {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-key": "cc74283b3cmsha2d48cf300f3721p1c76adjsn38941aea8e35",
+                "x-rapidapi-host": "amazon-price1.p.rapidapi.com"
+            }
+        })
+        const data = await response.json();
+        console.log(data);
+        ctx.body = JSON.stringify(data);
+    } catch (e) {
+        console.log(e);
+    }
 })
 
 export default router;
